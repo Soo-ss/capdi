02355494336828417d6a7a838b5d95039b0c29d8
HaenooKim <68438969+HaenooKim@users.noreply.github.com>
2021-05-02 21:53:42 +0900

    menu complete

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "../../partials/Header";
 import "./menuStyle.scss";
-import mmse from "./img/abc.png";
-
-
+import mmse from "./img/mmse.png";
+import clock from "./img/clock.png";
+import preventive from "./img/preventive.png";
 
 function Menu() {
   return (
@@ -11,41 +11,44 @@ function Menu() {
     // 메뉴창
     <div className="container">
       <h1 className="menu__title">메뉴</h1>
-      
-      
-
       <div className="mmse">
-        <img src={mmse} alt='mypic' />
-        <div className="mmse__discription">
-          <a>MMSE-M검사</a>
-          <p className="discription">5분만에 치매를 판단할 수 있습니다.</p>
+        <div className="mmse__menu">
+          <img src={mmse} alt='mypic' className="logo__img"/>
+          <div className="menu__discription">
+            <p className="discription__title">MMSE-M검사</p>
+            <p className="discription">설문 형식의 테스트로, 
+            5분만에 치매를 진단할 수 있습니다.</p>
+          </div>
         </div>
-
         <button className="right__button1">
           <i className="fas fa-angle-right"></i>
         </button>
-
       </div>
-
 
       <div className="clock">
-        <div className="clock__discription">
-          <a>시계그리기 검사</a>
-          <p className="discription">간단한 시계그리기로 치매를 판단할 수 있습니다.</p>
-          <button className="right__button2">
-            <i className="fas fa-angle-right"></i>
-          </button>
-        </div>
-
-      </div>
-      <div className="preventive">
-        <div className="prventive__discription">
-            <a>치매예방법 소개</a>
-            <p className="discription">치매를 예방할 수 있는 방법을 소개합니다.</p>
-            <button className="right__button3">
-              <i className="fas fa-angle-right"></i>
-            </button>
+        <div className="clock__menu">
+          <img src={clock} alt='mypic' className="logo__img"/>
+          <div className="menu__discription">
+            <p className="discription__title">시계그리기 검사</p>
+            <p className="discription">시계그리기 검사를 통해 치매를 진단할 수 있습니다.</p>
           </div>
+        </div>
+        <button className="right__button2">
+          <i className="fas fa-angle-right"></i>
+        </button>
+      </div>
+
+      <div className="preventive">
+        <div className="preventive__menu">
+          <img src={preventive} alt='mypic' className="logo__img"/>
+          <div className="menu__discription">
+            <p className="discription__title">치매 예방법</p>
+            <p className="discription">다양한 치매예방법을 소개합니다.</p>
+          </div>
+        </div>
+        <button className="right__button3">
+          <i className="fas fa-angle-right"></i>
+        </button>
       </div>
     </div>
   );
