ae9890bae4715e5b6e40663c290f2fceea56cefa
Kyungyun Kim <kyknn79@naver.com>
2021-05-05 22:28:23 +0900

    add img upload temporarily

--- src/pages/ClockCheck/ClockCheck.tsx
+++ src/pages/ClockCheck/ClockCheck.tsx
@@ -1,13 +1,98 @@
 import React from "react";
+import $ from "jquery";
+import Layout from "../../partials/Layout";
 
 // rafce
+
 const ClockCheck = () => {
+  function readURL(input: any) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $(".image-upload-wrap").hide();
+
+        $(".file-upload-image").attr("src");
+        $(".file-upload-content").show();
+
+        $(".image-title").html(input.files[0].name);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    } else {
+      removeUpload();
+    }
+  }
+
+  function removeUpload() {
+    $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+    $(".file-upload-content").hide();
+    $(".image-upload-wrap").show();
+  }
+  $(".image-upload-wrap").bind("dragover", function () {
+    $(".image-upload-wrap").addClass("image-dropping");
+  });
+  $(".image-upload-wrap").bind("dragleave", function () {
+    $(".image-upload-wrap").removeClass("image-dropping");
+  });
+
   return (
-    <div className="container">
-      <h1 style={{ fontSize: "50px" }}>바꿨음</h1>
-      <div style={{ height: "500px", backgroundColor: "#888" }}></div>
-    </div>
+    <Layout>
+      <script
+        className="jsbin"
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+      <div className="file-upload">
+        <button
+          className="file-upload-btn"
+          type="button"
+          onClick={(e) => $(".file-upload-input").trigger("click")}>
+          Add Image
+        </button>
+
+        <div className="image-upload-wrap">
+          <input
+            className="file-upload-input"
+            type="file"
+            onChange={(e) => readURL(this)}
+            accept="image/*"
+          />
+          <div className="drag-text">
+            <h3>Drag and drop a file or select add Image</h3>
+          </div>
+        </div>
+        <div className="file-upload-content">
+          <img className="file-upload-image" src="#" alt="your image" />
+          <div className="image-title-wrap">
+            <button
+              type="button"
+              onClick={(e) => removeUpload()}
+              className="remove-image">
+              Remove <span className="image-title">Uploaded Image</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
+
+  // return (
+  //   <Layout>
+  //     <div className="control">
+  //       <div className="align">
+  //         <button id="button_repeat" onClick={(e) => console.log("onClick")}>
+  //           Click
+  //         </button>
+  //       </div>
+
+  //       <div className="align">
+  //         <div>
+  //           <b>Event Trace</b>
+  //         </div>
+  //         <div className="eventTrace"></div>
+  //       </div>
+  //     </div>
+  //   </Layout>
+  // );
 };
 
 export default ClockCheck;
