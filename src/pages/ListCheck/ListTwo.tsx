cee41ca9624672fe1211c22ed62ebb7d91e7ff7d
Liz <lizpark811@gmail.com>
2021-05-08 00:22:48 +0900

    two fixed for not having empty line & sixteen fixed for duplicated question with four

--- src/pages/ListCheck/ListTwo.tsx
+++ src/pages/ListCheck/ListTwo.tsx
@@ -7,7 +7,7 @@ function ListCheck2() {
         {" "}
         {"<"}문항 2{">"}{" "}
       </div>
-
+      <br />
       <div className="question">먹고 싶지 않았다 ; 입맛이 없었다.</div>
       <div className="questions">
         <input type="radio" name="answer2" value="rarely" /> 극히 드물다 <br />
