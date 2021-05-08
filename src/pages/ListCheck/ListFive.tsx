96a171b619ebc6a4db5c632892c45402267d64a3
Liz <lizpark811@gmail.com>
2021-05-08 15:09:06 +0900

    Calc page made

--- src/pages/ListCheck/ListFive.tsx
+++ src/pages/ListCheck/ListFive.tsx
@@ -11,16 +11,16 @@ function ListCheck5() {
       <div className="question">무슨 일을 하든 정신을 집중하기 힘들었다.</div>
       <br />
       <div className="questions">
-        <input type="radio" name="answer" value="rarely" /> 극히 드물다 <br />
+        <input type="radio" name="answer5" value="rarely" /> 극히 드물다 <br />
       </div>
       <div className="questions">
-        <input type="radio" name="answer" value="sometimes" /> 가끔 <br />
+        <input type="radio" name="answer5" value="sometimes" /> 가끔 <br />
       </div>
       <div className="questions">
-        <input type="radio" name="answer" value="often" /> 자주 <br />
+        <input type="radio" name="answer5" value="often" /> 자주 <br />
       </div>
       <div className="questions">
-        <input type="radio" name="answer" value="mostly" /> 거의 대부분 <br />
+        <input type="radio" name="answer5" value="mostly" /> 거의 대부분 <br />
       </div>
     </div>
   );
--- src/pages/ListCheck/ListSix.tsx
+++ src/pages/ListCheck/ListSix.tsx
@@ -11,16 +11,16 @@ function ListCheck6() {
       <div className="question">우울했다.</div>
       <br />
       <div className="questions">
-        <input type="radio" name="answer" value="rarely" /> 극히 드물다 <br />
+        <input type="radio" name="answer6" value="rarely" /> 극히 드물다 <br />
       </div>
       <div className="questions">
-        <input type="radio" name="answer" value="sometimes" /> 가끔 <br />
+        <input type="radio" name="answer6" value="sometimes" /> 가끔 <br />
       </div>
       <div className="questions">
-        <input type="radio" name="answer" value="often" /> 자주 <br />
+        <input type="radio" name="answer6" value="often" /> 자주 <br />
       </div>
       <div className="questions">
-        <input type="radio" name="answer" value="mostly" /> 거의 대부분 <br />
+        <input type="radio" name="answer6" value="mostly" /> 거의 대부분 <br />
       </div>
     </div>
   );
