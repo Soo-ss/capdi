1ab7b63fef6720df53416c44f60aa633b1c16d38
Kyungyun Kim <kyknn79@naver.com>
2021-05-08 16:34:19 +0900

    footer style add

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,20 +1,43 @@
 import React from "react";
+import "../scss/footerStyle.scss";
+import exam from "./img/exam.png";
+import clock from "./img/clock.png";
+import preventive from "./img/preventive.png";
+import back from "./img/back.png";
+import { Link, useHistory } from "react-router-dom";
 
 const Footer = () => {
+  const history = useHistory();
+
+  const goBack = () => {
+    history.goBack();
+  };
   return (
-    <div
-      style={{
-        height: "60px",
-        width: "100%",
-        position: "fixed",
-        background: "linear-gradient(#7f00ff, #e100ff)",
-        left: "0",
-        bottom: "0",
-      }}>
-      <h1 style={{ color: "#fff", fontSize: "15px" }}>
-        푸터에 메뉴를 넣을 예정이지만 필요없으면 안넣어도 됩니다.
-      </h1>
-      {/* <input type="text" /> */}
+    <div className="footer">
+      <Link to="/list-check">
+        <div>
+          <img src={exam} alt="mmse test" className="icon_img" />
+        </div>
+      </Link>
+      <Link to="/clock">
+        <div>
+          <img src={clock} alt="clock test" className="icon_img" />
+        </div>
+      </Link>
+      <Link to="/preventive">
+        <div>
+          <img src={preventive} alt="preventive" className="icon_img" />
+        </div>
+      </Link>
+
+      <div className="back_icon">
+        <img
+          src={back}
+          alt="back button"
+          className="icon_img"
+          onClick={goBack}
+        />
+      </div>
     </div>
   );
 };
