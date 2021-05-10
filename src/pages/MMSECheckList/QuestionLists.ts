a585f59ca54bd524ca229dcf06f613e05f9e38fd
Liz <lizpark811@gmail.com>
2021-05-10 19:31:22 +0900

    mmse questions fixed

--- src/pages/MMSECheckList/QuestionLists.ts
+++ src/pages/MMSECheckList/QuestionLists.ts
@@ -6,102 +6,127 @@ export const lists = [
   },
   {
     listNumber: "문항 1",
-    title: "평소에는 아무렇지도 않던 일들이 귀찮게 느껴졌다.",
+    title: "올해는 몇 년도 입니까?",
     nextPage: "q2",
   },
   {
     listNumber: "문항 2",
-    title: "먹고 싶지 않았다 ; 입맛이 없었다.",
+    title: "지금은 무슨 계절입니까?",
     nextPage: "q3",
   },
   {
     listNumber: "문항 3",
-    title: "가족이나 친구가 도와주더라도 울적한 기분을 떨쳐 버릴 수 없었다.",
+    title: "오늘은 며칠입니까?",
     nextPage: "q4",
   },
   {
     listNumber: "문항 4",
-    title: "다른 사람들만큼 능력이 있다고 느꼈다.",
+    title: "오늘은 무슨 요일입니까?",
     nextPage: "q5",
   },
   {
     listNumber: "문항 5",
-    title: "무슨 일을 하든 정신을 집중하기 힘들었다.",
+    title: "지금은 몇 월입니까?",
     nextPage: "q6",
   },
   {
     listNumber: "문항 6",
-    title: "우울했다.",
+    title: "우리가 있는 이곳은 무슨 도/특별시/광역시입니까?",
     nextPage: "q7",
   },
   {
     listNumber: "문항 7",
-    title: "하는 일마다 힘들게 느껴졌다.",
+    title: "여기는 무슨 시/군입니까?",
     nextPage: "q8",
   },
   {
     listNumber: "문항 8",
-    title: "미래에 대하여 희망적이라고 느꼈다.",
+    title: "여기는 무슨 구/동/읍/면입니까?",
     nextPage: "q9",
   },
   {
     listNumber: "문항 9",
-    title: "내 인생은 실패작이라는 생각이 들었다.",
+    title: "우리는 지금 이 건물의 몇 층에 있습니까?",
     nextPage: "q10",
   },
   {
     listNumber: "문항 10",
-    title: "두려움을 느꼈다.",
+    title: "이 장소의 이름이 무엇입니까?",
     nextPage: "q11",
   },
   {
     listNumber: "문항 11",
-    title: "잠을 설쳤다 ; 잠을 잘 이루지 못했다.",
+    title:
+      "제가 세 가지 물건의 이름을 말씀드리겠습니다. 끝까지 다 들으신 다음에 세 가지 물건의 이름ㅇ르 모두 말씀해 보십시오. 그리고 몇 분 후에 그 세 가지 물건의 이름들을 다시 물어볼 것이니 들으신 물건의 이름을 잘 기억하십시오.",
     nextPage: "q12",
   },
   {
     listNumber: "문항 12",
-    title: "행복했다.",
+    title: "100에서 7을 빼면 얼마가 됩니까?",
     nextPage: "q13",
   },
   {
-    listNumber: "문항 13",
-    title: "평소보다 말을 적게 했다 ; 말수가 줄었다.",
+    listNumber: "문항 12-1",
+    title: "거기에서 7을 빼면 얼마가 됩니까?",
     nextPage: "q14",
   },
   {
-    listNumber: "문항 14",
-    title: "세상에 홀로 있는 듯한 외로움을 느꼈다.",
+    listNumber: "문항 12-2",
+    title: "거기에서 7을 빼면 얼마가 됩니까?",
     nextPage: "q15",
   },
   {
-    listNumber: "문항 15",
-    title: "사람들이 나에게 차갑게 대하는 것 같았다.",
+    listNumber: "문항 12-3",
+    title: "거기에서 7을 빼면 얼마가 됩니까?",
     nextPage: "q16",
   },
   {
-    listNumber: "문항 16",
-    title: "생활이 즐거웠다.",
+    listNumber: "문항 12-4",
+    title: "거기에서 7을 빼면 얼마가 됩니까?",
     nextPage: "q17",
   },
   {
-    listNumber: "문항 17",
-    title: "갑자기 울음이 나왔다.",
+    listNumber: "문항 13",
+    title:
+      "조금 전 제가 기억하라고 말씀드렸던 세 가지 물건의 이름이 무엇인지 말씀하여 주십시오.",
     nextPage: "q18",
   },
   {
-    listNumber: "문항 18",
-    title: "슬픔을 느꼈다.",
+    listNumber: "문항 14",
+    title: "(실제 시계를 보여주며) 이것을 무엇이라고 합니까?",
     nextPage: "q19",
   },
   {
-    listNumber: "문항 19",
-    title: "사람들이 나를 싫어하는 것 같았다.",
+    listNumber: "문항 14-2",
+    title: "(실제 연필을 보여주며) 이것을 무엇이라고 합니까?",
     nextPage: "q20",
   },
   {
-    listNumber: "문항 20",
-    title: "도무지 무엇을 시작할 기운이 나지 않았다.",
+    listNumber: "문항 15",
+    title:
+      "제가 하는 말을 끝까지 듣고 따라해 보십시오. 한 번만 말씀드릴 것이니 잘 듣고 따라 하십시오.",
+    nextPage: "result",
+  },
+  {
+    listNumber: "문항 16",
+    title:
+      "지금부터 제가 말씀드리는 대로 해 보십시오. 한 번만 말씀드릴 것이니 잘 들으시고 그대로 해 보십시오.",
+    nextPage: "result",
+  },
+  {
+    listNumber: "문항 17",
+    title:
+      "(겹친 오각형 그림을 가리키며) 여기에 오각형이 겹쳐져 있는 그림이 있습니다. 이 그림을 아래 빈 곳에 그대로 그려보십시오.",
+    nextPage: "result",
+  },
+  {
+    listNumber: "문항 18",
+    title: "옷은 왜 빨아서 입습니까?",
+    nextPage: "result",
+  },
+  {
+    listNumber: "문항 19",
+    title: '"티끌 모아 태산"은 무슨 뜻입니까?',
     nextPage: "result",
   },
 ];
