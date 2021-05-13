ac2df19448c5754224e529bef574b75340335c44
Kyungyun Kim <kyknn79@naver.com>
2021-05-13 21:55:27 +0900

    result pages modify

--- src/pages/ResultPage/MMSEResult.tsx
+++ src/pages/ResultPage/MMSEResult.tsx
@@ -9,17 +9,24 @@ const MMSEResult = () => {
 
   const render_MMSEResult = () => {
     if (mmseScore >= 24) {
-      return <p className="result_description">정상입니다.</p>;
+      return (
+        <p className="result_description">
+          정상입니다. 정확한 결과를 위해 시계그리기 검사까지 해보는 것을
+          권장합니다.
+        </p>
+      );
     } else if (mmseScore <= 23 && mmseScore >= 20) {
       return (
         <p className="result_description">
-          치매가 의심됩니다. 정밀검사를 받아보세요.
+          치매가 의심됩니다. 정밀검사를 받아보세요. 정확한 결과를 위해
+          시계그리기 검사까지 받아보는 것을 권장합니다.
         </p>
       );
     } else if (mmseScore < 20) {
       return (
         <p className="result_description">
-          치매로 판단됩니다. 인근 병원에서 진료를 받기 바랍니다.
+          치매로 판단됩니다. 인근 병원에서 진료를 받기 바랍니다. 정확한 결과를
+          위해 시계그리기 검사까지 받아보는 것을 권장합니다.
         </p>
       );
     }
@@ -34,6 +41,9 @@ const MMSEResult = () => {
       <div className="return_test">
         <Link to="/mq1">
           <button className="submit_btn">다시 테스트하기</button>
+        </Link>
+        <Link to="/userInput">
+          <button className="submit_btn">시계그리기 검사하기</button>
         </Link>
       </div>
     </Layout>
