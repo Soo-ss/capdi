89d5546b7603c582b5d8c65f294a354d11892d7b
HaenooKim <68438969+HaenooKim@users.noreply.github.com>
2021-05-13 10:10:28 +0900

    modify button

--- src/pages/MMSECheckList/Surveys/userInput.tsx
+++ src/pages/MMSECheckList/Surveys/userInput.tsx
@@ -6,13 +6,12 @@ import "../listStyle.scss";
 const userInput = ()=> {
     return  (
         <Layout>
-            <Link to="/mq1">
                 <h1 className="notice_title">{"< "}주의사항{" >"}</h1>
                 <div className="MMSE_notice">
                     <ul>
                         <li>1. 보호자가 반드시 필요합니다.</li>
                         <li>2. 검사자의 학력, 성별, 나이에 따라 검사기준이 달라지므로 정확히 입력해주시기 바랍니다.</li>
-                        <li></li>
+                        <li>3. 우울증이 있을경우 치매검사가 정확하지 않을 수 있습니다. 우울증 검사를 하고 난 뒤 하는 것을 권장합니다.</li>
                         <li></li>
                         <li></li>
                     </ul>
@@ -23,6 +22,7 @@ const userInput = ()=> {
                 </div>
 
 
+            <Link to="/mq1">
                 <div className="go_MMSE">
                     MMSE검사 시작하기
                 </div>
